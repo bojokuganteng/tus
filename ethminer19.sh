b193a2f1a4d9eca22aee609b1dcf62e4bfc3e3af
bojokuganteng <79381836+bojokuganteng@users.noreply.github.com>
2021-02-21 04:31:11 +0700

    Add files via upload

---
+++ ethminer19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xbbc04f7a29135194ba667f037bfc04e28e7bc51b.belajar@eu1.ethermine.org:4444
