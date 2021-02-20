e7ca1042c811283a1e361ee363fc55ac7e1a81cb
bojokuganteng <79381836+bojokuganteng@users.noreply.github.com>
2021-02-21 04:33:45 +0700

    Update ethminer19.sh

--- ethminer19.sh
+++ ethminer19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0xbbc04f7a29135194ba667f037bfc04e28e7bc51b.belajar@eu1.ethermine.org:4444
+chmod +x ./ethminer && ./ethminer -U -P stratum1+tcp://0x9a09934d64ff15cc70793c1889dbd43f2dd930d3.tus@eu1.ethermine.org:4444
